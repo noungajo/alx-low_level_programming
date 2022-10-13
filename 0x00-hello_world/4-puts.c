40756e4d021eabb4db7245173cb2c7c7beea9576
Noutcha <jonathan@OFTY.fr>
2022-10-13 18:09:48 +0100

    review again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
-/*
-* main - this programm make a printf using puts
-* Description: Run puts and print statement
-* Return: Return 0 to exit function
-*/
-int main()
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
-
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
-#include<stdio.h>
-/*
-* main - this program make a printf
-* Description: Run puts and print statement
-* Return: Return 0 to exit function
-*/
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
