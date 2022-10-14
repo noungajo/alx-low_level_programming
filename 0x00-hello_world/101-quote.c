a3449bd9ef4dcba7b5320f0bc3b6c2bec3aea252
noutcha <noutcha.ofty@gmail.com>
2022-10-14 03:13:47 -0700

    betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int size = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	int standardError = 2;
-	write(standardError, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", size);
+	int standard_error = 2;
+	write(standard_error, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", size);
 	return (1);
 }
